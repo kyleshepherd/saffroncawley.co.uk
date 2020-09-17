ee5d9a4c14afc77877aa2209677a41f094b5038e
Kyle Shepherd <kyleshepherd13@gmail.com>
2020-09-17 14:41:41 +0100

    Created gallery page template

---
+++ wp-content/themes/devherd/template-gallery.php
@@ -0,0 +1,14 @@
+<?php /* Template Name: Gallery Page */ ?>
+
+<?php
+get_header();
+?>
+
+<main>
+	<?php get_template_part( 'inc/sidebar' ); ?>
+	<?php get_template_part( 'inc/block', 'masonry' ); ?>
+</main>
+
+<?php
+get_footer();
+?>
